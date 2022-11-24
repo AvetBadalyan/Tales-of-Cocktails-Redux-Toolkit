de70010b21e82c31cc62ade6bab6e65d84a87c5f
AvetBadalyan <avetbadalyan@gmail.com>
2022-11-24 19:06:26 +0400

    feat: add search input component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import SingleCocktail from "./pages/SingleCocktail/SingleCocktail";
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import SearchInput from '../../components/Search/SearchInput'
 
 export default function Home() {
   return (
       <div>
           <h2>Home</h2>
+          <SearchInput />
     </div>
   )
 }
